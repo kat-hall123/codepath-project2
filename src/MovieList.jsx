8011ab33b3edfe9a86f91e7ffe39df356345cc88
Katherine Hall <khall206@meta.com>
2025-06-10 15:38:19 -0700

    implemented load more button and displaying additional movie pages

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -4,26 +4,43 @@ import './MovieList.css';
 
 function MovieList() {
     const [movies, setMovies] = useState([]);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
 
-    const fetchData = async () => {
+    const fetchData = async (pageNum) => {
         try {
             const apiKey = import.meta.env.VITE_API_KEY;
-            const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`);
+            const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=${pageNum}`);
             
             if (!response.ok) {
                 throw new Error('Failed to fetch movie data');
             }
             
             const data = await response.json();
-            setMovies(data.results);
+
+            if(data.page >= data.total_pages) {
+                setHasMore(false);
+            }
+
+            setMovies((prevMovies) => [...prevMovies, ...data.results]);
         } catch (error) {
             console.error(error);
         }
     };
 
     useEffect(() => {
-        fetchData();
+        fetchData(page);
     }, []);
+
+    useEffect(() => {
+        if(page > 1) {
+            fetchData(page);
+        }
+    }, [page]);
+
+    function handleLoadMore() {
+        return setPage((prevPage) => prevPage + 1);
+    }
 
     return(
         <div className="movie-list-container">
@@ -41,6 +58,14 @@ function MovieList() {
                     />
                 })}
             </main>
+            
+            {hasMore ? (
+                <button className="load-more-button" onClick={handleLoadMore}>
+                    Load More
+                </button>
+            ) : (
+                <p id="no-more">No more movies to show</p>
+            )}
         </div>
     );
 };
